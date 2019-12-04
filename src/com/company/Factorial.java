5e0948a927f4ac634d506a91f8b2f21e9780ccb1
student <evanlay@billericak12.com>
2019-12-04 12:06:30 -0500

    Factorial Commit

---
+++ src/com/company/Factorial.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+    }
+    public static void main() {
+
+    }
+    public static int calcFactorial(int x) {
+
+    }
+}
